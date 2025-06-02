99a2b47e3b8737257721656728c72b088b1143f7
jrkim-kr <jeongrankim99@gmail.com>
2025-06-02 20:53:15 +0900

    Update 조건에 부합하는 중고거래 댓글 조회하기.sql

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,17 @@
-SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, DATE_FORMAT(r.created_date, '%Y-%m-%d') AS created_date
-FROM used_goods_board b
-INNER JOIN used_goods_reply r
-ON b.board_id = r.board_id
-WHERE b.created_date LIKE '2022-10%'
-ORDER BY r.created_date ASC, b.title ASC
+SELECT 
+    b.title, 
+    board_id, 
+    r.reply_id, 
+    r.writer_id, 
+    r.contents, 
+    DATE_FORMAT(r.created_date, '%Y-%m-%d') AS created_date
+FROM 
+    used_goods_board b
+INNER JOIN 
+    used_goods_reply r
+USING (board_id)
+WHERE 
+    DATE_FORMAT(b.created_date, '%Y-%m') = '2022-10'
+ORDER BY 
+    r.created_date ASC, 
+    b.title ASC;
