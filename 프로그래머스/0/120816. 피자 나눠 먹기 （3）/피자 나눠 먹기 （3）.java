10c7580a07522f9dbb96fdc6e825f7ee8a445cb0
jrkim-kr <jeongrankim99@gmail.com>
2025-06-05 21:26:17 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -2,7 +2,8 @@ class Solution {
     public int solution(int slice, int n) {
         int pizza = 1;
         
-        while ((pizza * slice) / n == 0) {
+        // 현재 피자로 모든 사람이 먹을 수 있을 때까지 반복
+        while (pizza * slice < n) {
             pizza++;
         }
         
