5482306e23970067bb3de3e0852c8032aa31f81c
jrkim-kr <jeongrankim99@gmail.com>
2025-06-13 19:13:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.59 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,15 +1,15 @@
 import java.util.*;
 import java.util.stream.Collectors;
 class Solution {
-    public int[] solution(int[] emergency) {
-        List<Integer> sorted = Arrays.stream(emergency) // int[] → IntStream
-                                    .boxed() // IntStream -> Stream<Integer>
+    public int[] solution(int[] emergency) {        
+        List<Integer> sorted = Arrays.stream(emergency)                 // int[] → IntStream                   
+                                    .boxed()                            // IntStream → Stream<Integer>
                                     .sorted(Collections.reverseOrder()) 
-                                    .collect(Collectors.toList()); // Stream<Integer> -> List<Integer>
+                                    .collect(Collectors.toList());      // Stream<Integer> → List<Integer>
             
         
         return Arrays.stream(emergency)
-                    .map(e -> sorted.indexOf(e) + 1) // int -> Stream<Integer>
-                    .toArray(); // Stream<Integer> -> int[]
+                    .map(e -> sorted.indexOf(e) + 1)                    // int → Stream<Integer>
+                    .toArray();                                         // Stream<Integer> → int[]
     }
 }
