fa928c2842f89fde848a18eed1da16a0b3956c4a
jrkim-kr <jeongrankim99@gmail.com>
2025-06-13 19:32:17 +0900

    [level 0] Title: 순서쌍의 개수, Time: 13.62 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,11 +1,6 @@
+import java.util.stream.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer++;
-            }
-        }
-        return answer;
+        return (int) IntStream.rangeClosed(1,n).filter(i -> n % i == 0).count();
     }
 }
