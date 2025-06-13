12bef4323bb426e4940d98b3f2ccc86d3c0e2eae
jrkim-kr <jeongrankim99@gmail.com>
2025-06-13 20:19:17 +0900

    [level 0] Title: 모스부호 (1), Time: 12.49 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,8 +1,7 @@
 import java.util.*;
 class Solution {
     public String solution(String letter) {
-        // 문제에서 제공된 모스부호 배열 (a~z 순서)
-        String[] morseArray = {
+        String[] morse = {
             ".-", "-...", "-.-.", "-..", ".", "..-.", 
             "--.", "....", "..", ".---", "-.-", ".-..", 
             "--", "-.", "---", ".--.", "--.-", ".-.", 
@@ -10,21 +9,16 @@ class Solution {
             "-.--", "--.."
         };
         
-        // HashMap에 모스부호와 알파벳 매핑
-        HashMap<String, Character> morse = new HashMap<>();
-        for (int i = 0; i < morseArray.length; i++) {
-            morse.put(morseArray[i], (char)('a' + i));
+        String result = "";
+        for (String code : letter.split(" ")) {
+            for (int i = 0; i < morse.length; i++) {
+                if (morse[i].equals(code)) {
+                    result += (char)('a' + i);
+                    break;
+                }
+            }
         }
-        
-        // 입력 문자열 분할 및 변환
-        StringBuilder answer = new StringBuilder();
-        String[] inputArray = letter.split(" ");
-        
-        for (String morseCode : inputArray) {
-            answer.append(morse.get(morseCode));
-        }
-        
-        return answer.toString();
+        return result;
     }
 }
 
