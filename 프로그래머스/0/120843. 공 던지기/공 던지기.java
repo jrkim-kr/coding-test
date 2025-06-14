20ee72a533ac3a4c329a7033b20f730612b0f9ee
jrkim-kr <jeongrankim99@gmail.com>
2025-06-14 17:27:29 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int idx = 0;
-        
-        for ( int i = 1; i < k; i++) {
-            idx = (idx + 2) % numbers.length;
-        }
-        
-        return numbers[idx];
+
+        return numbers[(2 * (k - 1)) % numbers.length];
     }
 }
