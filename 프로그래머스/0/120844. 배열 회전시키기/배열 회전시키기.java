c6bb8b0d0b5b4cb1540692175fe41446b8b578ac
jrkim-kr <jeongrankim99@gmail.com>
2025-06-14 18:15:00 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.02 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,18 +1,22 @@
-class Solution {
+public class Solution {
     public int[] solution(int[] numbers, String direction) {
         int[] answer = new int[numbers.length];
         
         if (direction.equals("right")) {
+            // 1. 마지막 원소를 맨 앞에 배치
             answer[0] = numbers[numbers.length - 1];
-            for (int i = 1; i < numbers.length; i++) {
-                answer[i] = numbers[i - 1];
-            }
+            
+            // 2. 나머지 원소들을 한 번에 복사 (0번째부터 length-1개를 1번째 위치부터 복사)
+            System.arraycopy(numbers, 0, answer, 1, numbers.length - 1);
+            
         } else {
+            // 1. 두 번째 원소부터 마지막까지를 앞쪽으로 복사 (1번째부터 length-1개를 0번째 위치부터 복사)
+            System.arraycopy(numbers, 1, answer, 0, numbers.length - 1);
+            
+            // 2. 첫 번째 원소를 맨 뒤에 배치
             answer[numbers.length - 1] = numbers[0];
-            for (int i = 0; i < numbers.length - 1; i++) {
-                answer[i] = numbers[i + 1];
-            }
         }
+        
         return answer;
     }
 }
