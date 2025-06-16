efe8833e7b4f3dbc6c54a992a9ecbc367c781412
jrkim-kr <jeongrankim99@gmail.com>
2025-06-16 17:08:14 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,14 @@
 class Solution {
     public String solution(String my_string) {
-        return my_string.replace("a", "")
-                        .replace("e", "")
-                        .replace("i", "")
-                        .replace("o", "")
-                        .replace("u", "");
+        StringBuilder answer = new StringBuilder();
+        
+        for (char c : my_string.toCharArray()) {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
     }
 }
 
