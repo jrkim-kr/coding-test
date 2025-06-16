295560e763f21e934db956e2203f7b286a04d36d
jrkim-kr <jeongrankim99@gmail.com>
2025-06-16 20:30:12 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,14 +1,12 @@
-SELECT a.grade, a.id, a.email
-FROM (
-    SELECT
-        CASE
-            WHEN skill_code & (SELECT (SUM(code)) FROM skillcodes WHERE category = 'Front End') > 0 AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') > 0 THEN 'A'
-            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') > 0 THEN 'B'
-            WHEN skill_code & (SELECT (SUM(code)) FROM skillcodes WHERE category = 'Front End') > 0 THEN 'C'
-            ELSE NULL
-        END AS grade,
-        id,
-        email
-    FROM developers) a
-WHERE a.grade IS NOT NULL
-ORDER BY a.grade ASC, id ASC
+SELECT 
+    CASE
+        WHEN skill_code & (SELECT (SUM(code)) FROM skillcodes WHERE category = 'Front End') > 0 AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') > 0 THEN 'A'
+        WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') > 0 THEN 'B'
+        WHEN skill_code & (SELECT (SUM(code)) FROM skillcodes WHERE category = 'Front End') > 0 THEN 'C'
+        ELSE NULL
+    END AS grade,
+    id,
+    email
+FROM developers
+HAVING grade IS NOT NULL
+ORDER BY grade ASC, id ASC
