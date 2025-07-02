5d98fbb9a5573c9aacf266f4ffffe3c899d90cca
jrkim-kr <jeongrankim99@gmail.com>
2025-07-02 16:40:17 +0900

    [level 0] Title: 자릿수 더하기, Time: 3.22 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n) {
+        return String.valueOf(n)
+                     .chars()
+                     .map(i -> i - '0')
+                     .sum();
+    }
+}
