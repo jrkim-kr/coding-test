52952a0e6d41283018a62a2a41b887dc239f06e8
jrkim-kr <jeongrankim99@gmail.com>
2025-07-21 20:11:44 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int coupons = chicken;
         
-        while (coupons >= 10) {
-            answer += coupons / 10;
-            coupons = coupons % 10 + coupons / 10;
+        while (chicken >= 10) {
+            answer += chicken / 10;
+            chicken = chicken % 10 + chicken / 10;
         }
         return answer;
     }
