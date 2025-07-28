095c957a93ba1531430cb19c456856169bedcda5
jrkim-kr <jeongrankim99@gmail.com>
2025-07-28 12:42:14 +0900

    [level 0] Title: 문자열 밀기, Time: 1.81 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,5 @@
 class Solution {
     public int solution(String A, String B) {
-        if (A.equals(B)) {
-            return 0;
-        }
-        
-        String currStr = A;
-        for (int i = 1; i < A.length(); i++) {
-            currStr = currStr.charAt(A.length() - 1) + currStr.substring(0, A.length() - 1);
-            
-            if (currStr.equals(B)) {
-                return i;
-            }
-        }
-        return -1;
+        return (B + B).indexOf(A);
     }
 }
