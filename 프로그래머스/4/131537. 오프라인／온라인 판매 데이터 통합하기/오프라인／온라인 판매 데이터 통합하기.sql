e1f710f8240c0b1230dbe2452df9047481b3f6f2
jrkim-kr <jeongrankim99@gmail.com>
2025-08-20 18:49:08 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,16 +1,22 @@
-(SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, 
-        product_id, 
-        user_id, 
-        sales_amount
- FROM online_sale
- WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03')
- 
+(SELECT 
+    DATE_FORMAT(sales_date, "%Y-%m-%d") AS sales_date, 
+    product_id, 
+    user_id, 
+    sales_amount
+FROM online_sale
+WHERE YEAR(sales_date) = 2022 AND MONTH(sales_date) = 3
+GROUP BY sales_date, product_id, user_id)
+
 UNION
 
-(SELECT DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, 
-        product_id, 
-        NULL AS user_id,
-        sales_amount
- FROM offline_sale
- WHERE DATE_FORMAT(sales_date, '%Y-%m') = '2022-03')
-ORDER BY sales_date ASC, product_id ASC, user_id ASC;
+(SELECT 
+    DATE_FORMAT(sales_date, "%Y-%m-%d") AS sales_date, 
+    product_id, 
+    NULL AS user_id, 
+    sales_amount
+FROM offline_sale
+WHERE YEAR(sales_date) = 2022 AND MONTH(sales_date) = 3
+GROUP BY sales_date, product_id)
+
+ORDER BY sales_date ASC, product_id ASC, user_id ASC
+
